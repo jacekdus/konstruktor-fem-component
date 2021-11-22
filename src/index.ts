4624e4442fa7c029fa65fb3d10f90f60ab8a39f5
dus_j <jacekdus1990@gmail.com>
2021-11-22 09:19:01 +0100

    fix dist

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import View from "./App/View";
 import { config } from "./App/Config/config";
 import Controller from "./App/Controller";
 
-// import { model } from './Test/Models/0_empty'
-import { model } from './Test/Models/1_short_truss_roof'
+import { model } from './Test/Models/0_empty'
+// import { model } from './Test/Models/1_short_truss_roof'
 // import { model } from './Test/Models/2_big_truss_bridge'
 
 const view = new View(config);
